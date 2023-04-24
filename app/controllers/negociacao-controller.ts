286220fa5b3af1abd111a69921fc9c05292aaa01
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-04-24 01:44:46 -0300

    criando arquivo de negociações contendo uma lista imutável de negociações

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
+import { Negociacoes } from "../models/negociacoes.js";
 import { Negociacao } from "./../models/negociacao.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes();
 
   constructor() {
     this.inputData = document.querySelector("#data");
@@ -14,7 +16,9 @@ export class NegociacaoController {
   public add(): void {
     const negociacao = this.criaNegociacao();
 
-    console.log(negociacao);
+    this.negociacoes.adiciona(negociacao);
+    
+    console.log(this.negociacoes.lista());
 
     this.limparForm();
   }
